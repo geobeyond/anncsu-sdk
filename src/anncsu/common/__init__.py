534a1634a6648744b7e0d94cef2755452317b37f
Francesco Bartoli <xbartolone@gmail.com>
2025-11-11 13:31:59 +0100

    Add more tests for validation of inputs and outputs

--- src/anncsu/common/__init__.py
+++ src/anncsu/common/__init__.py
@@ -1,1 +1,15 @@
 """ANNCSU Common - Shared primitives for all ANNCSU API SDKs."""
+
+from anncsu.common.validation import (
+    ResponseValidator,
+    ValidationConfig,
+    base64_validator,
+    belfiore_code_validator,
+)
+
+__all__ = [
+    "ResponseValidator",
+    "ValidationConfig",
+    "base64_validator",
+    "belfiore_code_validator",
+]
